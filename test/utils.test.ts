64298af2e1878b1b1527c5d0dcac140151888598
Jakub Švehla <svehl.jakub@gmail.com>
2021-11-18 14:42:12 +0100

    fix spaces

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -2,58 +2,58 @@ import { mapObjectValues } from '../src/useFormio';
 
 describe('mapObjectValues', () => {
   it('check order 1', async () => {
-		const data = {
-			a: 'a',
-			b: 'b',
-			c: 'c',
-		}
+    const data = {
+      a: 'a',
+      b: 'b',
+      c: 'c',
+    }
 
-		const order = [] as string[]
-		mapObjectValues(
-			(_v, k) => {
-				order.push(k)
-			},
-			data,
-			{ stableKeyOrder: true }
-		)
+    const order = [] as string[]
+    mapObjectValues(
+      (_v, k) => {
+        order.push(k)
+      },
+      data,
+      { stableKeyOrder: true }
+    )
 
     expect(order).toEqual(['a', 'b', 'c']);
   });
 
   it('check order 2', async () => {
-		const data = {
-			c: 'c',
-			b: 'b',
-			a: 'a',
-		}
+    const data = {
+      c: 'c',
+      b: 'b',
+      a: 'a',
+    }
 
-		const order = [] as string[]
-		mapObjectValues(
-			(_v, k) => {
-				order.push(k)
-			},
-			data,
-			{ stableKeyOrder: true }
-		)
+    const order = [] as string[]
+    mapObjectValues(
+      (_v, k) => {
+        order.push(k)
+      },
+      data,
+      { stableKeyOrder: true }
+    )
 
     expect(order).toEqual(['a', 'b', 'c']);
   });
 
   it('stable order 3', async () => {
-		const data = {
-			b: 'b',
-			a: 'a',
-			c: 'c',
-		}
+    const data = {
+      b: 'b',
+      a: 'a',
+      c: 'c',
+    }
 
-		const order = [] as string[]
-		mapObjectValues(
-			(_v, k) => {
-				order.push(k)
-			},
-			data,
-			{ stableKeyOrder: true }
-		)
+    const order = [] as string[]
+    mapObjectValues(
+      (_v, k) => {
+        order.push(k)
+      },
+      data,
+      { stableKeyOrder: true }
+    )
 
     expect(order).toEqual(['a', 'b', 'c']);
   });
